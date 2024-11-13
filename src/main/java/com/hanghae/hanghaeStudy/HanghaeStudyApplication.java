e4e4e8efa089dfbff6475e288a13bee038edd5f2
hwa.j <hwajin0720@naver.com>
2024-11-14 00:04:08 +0900

    init: 프로젝트 및 DB 세팅

---
+++ src/main/java/com/hanghae/hanghaeStudy/HanghaeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaeStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeStudyApplication.class, args);
+	}
+
+}
