7fa38418c30e818c84f2b6829edec2ac1ebd221f
hwa.j <hwajin0720@naver.com>
2024-11-14 00:12:51 +0900

    Feat: User, Board entity 및 repository 생성

---
+++ src/main/java/com/hanghae/hanghaeStudy/entity/Board.java
@@ -0,0 +1,35 @@
+package com.hanghae.hanghaeStudy.entity;
+
+import com.hanghae.hanghaeStudy.entity.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Builder
+    public Board(User user, String title, String content){
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/hanghae/hanghaeStudy/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.hanghae.hanghaeStudy.repository;
+
+import com.hanghae.hanghaeStudy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
