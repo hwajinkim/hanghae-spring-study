142e69f28f6a56e1f81d155ce35dbc5e7dbc554a
hwajinkim <hwajin0720@naver.com>
2024-11-14 16:21:55 +0900

    Feat: 테스트코드 어노테이션 추가

--- src/test/java/com/hanghae/hanghaeStudy/HanghaeStudyApplicationTests.java
+++ src/test/java/com/hanghae/hanghaeStudy/HanghaeStudyApplicationTests.java
@@ -3,7 +3,7 @@ package com.hanghae.hanghaeStudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class HanghaeStudyApplicationTests {
 
 	@Test
